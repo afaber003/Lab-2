1e95e1784a18fd22ec873bf621edc0cbbe07edd2
Ben Bravo <>
2021-04-06 20:22:34 +0000

    Fixed the extraction operator function (output function) and implemented the push_back function

--- Intlist.cpp
+++ Intlist.cpp
@@ -21,17 +21,37 @@ void IntList::push_front(int value){
     newone->next->prev = newone;
 }
 
+void IntList::push_back(int value)
+{
+  IntNode* newone = new IntNode(value);
+  newone->prev = dummyTail->prev;
+  dummyTail->prev = newone;
+  newone->prev->next = newone;
+  newone->next = dummyTail;
+
+}
+
 ostream & operator<<(ostream &out, const IntList &rhs)
 {
   if (rhs.head != nullptr)
   {
     IntNode *tempNode = rhs.head;
-    while (tempNode != rhs.tail)
+    while (tempNode != nullptr)
     {
-      out << tempNode->data << " ";
+      if (tempNode == rhs.dummyHead || tempNode == rhs.dummyTail)
+      {
+      }
+      else if (tempNode != rhs.tail)
+      {
+        out << tempNode->data << " ";
+      }
+      else
+      {
+        out << tempNode->data;
+      }
       tempNode = tempNode->next;
     }
-    out << rhs.tail->data;
+    //out << rhs.dummyTail->prev->data;
   }
 
   return out;
--- main.cpp
+++ main.cpp
@@ -24,6 +24,15 @@ int main() {
   testList.push_front(1);
   cout << "testList: " << testList << endl;
 
+  cout << "Push back" << endl;
+
+  testList.push_back(20);
+  cout << "testList: " << testList << endl;
+  testList.push_back(3);
+  cout << "testList: " << testList << endl;
+  testList.push_back(2);
+  cout << "testList: " << testList << endl;
+
 
 
     return 0;
