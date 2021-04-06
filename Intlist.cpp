7e66d710d1224a6ef44358d9341477a10dd94de0
afaber003 <afaber003@gmail.com>
2021-04-06 11:00:57 -0700

    added push_front and destructor and added dummy nodes

--- Intlist.cpp
+++ Intlist.cpp
@@ -2,3 +2,20 @@
 
 using namespace std;
 
+IntList::~IntList(){
+    IntNode* curr = head;
+    IntNode* temp = nullptr;
+    while (curr != dummyHead and curr != dummyTail){
+        temp = curr->next;
+        delete curr;
+        curr = temp;
+    }
+}
+
+void IntList::push_front(int value){
+    IntNode* newone = new IntNode(value);
+    newone->next = dummyHead->next;
+    dummyHead->next = newone;
+    newone->prev = dummyHead;
+    newone->next->prev = newone;
+}
--- Intlist.h
+++ Intlist.h
@@ -5,23 +5,33 @@ struct IntNode {
     int data;
     IntNode *prev;
     IntNode *next;
-    IntNode(int data) : data(data), prev(0), next(0) {}
+    IntNode(int data) : data(data), prev(nullptr), next(nullptr) {}
 };
 
 class IntList {
 
     public:
         //default constructor
-        Intlist(){
-            IntNode* head = nullptr;
-            IntNode* tail - nullptr;
-            size = 0;
+        IntList(){
+            dummyHead = new IntNode(0);
+            dummyTail = new IntNode(0);
+
+            dummyHead->next = dummyTail;
+            dummyTail->prev = dummyHead;
+
+            head = dummyHead;
+            tail = dummyTail;
         }
+        
+        ~IntList();
+        void push_front(int);
 
 
     private:
-        IntNode* head = nullptr;
-        IntNode* tail = nullptr;
+        IntNode* dummyHead;
+        IntNode* dummyTail;
+        IntNode* head;
+        IntNode* tail;
         int size = 0; //added for extra help. I'm sure it will come in handy
 
 };
